2109340c134a7ce79a8bf820c45ccb9ebc40ae57
alexandre <alexandre.douchet.pro@gmail.com>
2019-07-12 16:36:09 +0200

    css /inscription

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -10,12 +10,12 @@
             </div>
             <div class="row justify-content-center">
                     <div class="col-8">
-                        <form class="" action="" method="post">
-                            <input class="form-control col" placeholder="Tapez votre nom" type="text" name="" id=""><br>
-                            <input class="form-control col" placeholder="Tapez votre prénom" type="text" name="" id=""><br>
-                            <input class="form-control col" placeholder="tapez votre adresse email" type="email" name="email" id=""><br>
-                            <input class="form-control col" placeholder="mot de passe" type="text" name="password1" id=""><br>
-                            <input class="form-control col" placeholder="mot de passe comfimation" type="text" name="password2" id=""><br>
+                        <form class="forminscription" action="" method="post">
+                            <input class="form-control col" placeholder="Tapez votre nom" type="text" name="" id="">
+                            <input class="form-control col" placeholder="Tapez votre prénom" type="text" name="" id="">
+                            <input class="form-control col" placeholder="tapez votre adresse email" type="email" name="email" id="">
+                            <input class="form-control col" placeholder="mot de passe" type="text" name="password1" id="">
+                            <input class="form-control col" placeholder="mot de passe comfimation" type="text" name="password2" id="">
                     </div>
             </div>
             <div class="row justify-content-center">
